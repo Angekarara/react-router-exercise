8c8c95d1abe2bb996ad80e41c7a227573ed76f22
Angekarara <mbabange2020@gmail.com>
2024-07-31 19:22:15 +0200

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./component/Home";
 import About from "./component/About";
+import Vans from "./component/Vans";
 function App() {
   return (
     <Router>
       <nav className="bg-[#FFF7ED] flex justify-between text-black font-bold py-9 px-2">
         <Link to="/">#VANILLE</Link>
-        <Link to="/about">About</Link>
+        <div className="flex justify-between gap-4">
+          <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
+        </div>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/Vans" element={<Vans />} />
       </Routes>
     </Router>
   );
---
+++ src/component/Vans.jsx
@@ -0,0 +1,8 @@
+function Vans() {
+  return (
+    <div className="">
+      <h1 className="text-3xl">This is van page 🚎🚎 </h1>
+    </div>
+  );
+}
+export default Vans;
